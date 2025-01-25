34f6c6224195906973e52ee5e439396aaa39d93a
Wade Good <wadegood14@gmail.com>
2025-01-25 18:11:38 +0000

    create strava class

---
+++ config_script/strava.py
@@ -0,0 +1,54 @@
+import json
+import requests
+
+#LOAD ENVIRONMENT VARIABLES FOR PROJECT
+with open('/workspaces/func-process-webhook-event/local.settings.json','r') as file:
+    data = json.load(file)
+
+STRAVA_CLIENT_ID = data['Values']['STRAVA_CLIENT_ID']
+STRAVA_CLIENT_SECRET = data['Values']['STRAVA_CLIENT_SECRET']
+
+class Strava:
+
+    def __init__(self):
+        self.client_id = STRAVA_CLIENT_ID
+        self.client_secret = STRAVA_CLIENT_SECRET
+
+    def view_subscriptions(self):
+        url = 'https://www.strava.com/api/v3/push_subscriptions'
+        params = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret
+        }
+        response = requests.get(url=url,params=params)
+        return response.content
+
+
+    def delete_subscription(self, subscription_id):
+        url = f'https://www.strava.com/api/v3/push_subscriptions/{subscription_id}'
+        params = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret
+        }
+        response = requests.delete(url=url,params=params)
+        return response.content
+
+    def create_subscription(self, callback_url, verify_token):
+        url = 'https://www.strava.com/api/v3/push_subscriptions'
+        params = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'callback_url': callback_url,
+            'verify_token': verify_token
+        }
+        response = requests.post(url=url,params=params)
+        return response.content
+
+   
+myStrava = Strava()
+
+result = myStrava.view_subscriptions()
+print(result)
+
+# result = myStrava.delete_subscription(272386)
+# print(result)
--- function_app.py
+++ function_app.py
@@ -1,25 +1,20 @@
 import azure.functions as func
 import logging
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="func_process_webhook_event")
 def func_process_webhook_event(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    hub_challenge = req.params.get('hub.challenge')
+    
+    if hub_challenge:
+        return func.HttpResponse(json.dumps({"hub.challenge":hub_challenge}),
+        status_code=200)
+    
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+             "Validation Failed. Please try again.",
+             status_code=401
         )
