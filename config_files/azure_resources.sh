887741191ca406e93c2704020887cef4a6dfae3b
Wade Good <wadegood14@gmail.com>
2025-01-29 05:34:38 +0000

    update azure script

--- config_files/azure_resources.sh
+++ config_files/azure_resources.sh
@@ -9,6 +9,9 @@ storage="dlsstravaevents$randomIdentifier"
 functionApp="func-process-webhook-event-$randomIdentifier"
 skuStorage="Standard_LRS"
 functionsVersion="4"
+runtime="python"
+runtimeVersion="3.11"
+os="linux"
 
 # Create a resource group
 echo "Creating $resourceGroup in "$location"..."
@@ -20,4 +23,4 @@ az storage account create --name $storage --location "$location" --resource-grou
 
 # Create a serverless function app in the resource group.
 echo "Creating $functionApp"
-az functionapp create --name $functionApp --storage-account $storage --consumption-plan-location "$location" --resource-group $resourceGroup --functions-version $functionsVersion
+az functionapp create --name $functionApp --storage-account $storage --consumption-plan-location "$location" --resource-group $resourceGroup --functions-version $functionsVersion --runtime $runtime --os-type $os --runtime-version $runtimeVersion
